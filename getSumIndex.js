0500209cd473e294e0585ad93597a6096c2ddcfc
HemaSaru <161929686+HemaSaru@users.noreply.github.com>
2025-10-22 22:23:22 +0530

    corrected code as it was failing

--- getSumIndex.js
+++ getSumIndex.js
@@ -7,27 +7,26 @@
 const targetValue = 6;
 let initValue = 0;
 let sumNum = 0;
-let retValue = [];
+let retValue = [0, 0];
 
 // const nums = [2, 7, 11, 15];
+// const nums = [2,5,5,11];
 const nums = [3,2,4];
 function getSum(nums, target) {
   for (let i = 0; i <= nums.length; i++) {
-    // Ind2 = i + 1;
-    sumNum = nums[i] + nums[i + 1];
+    initValue = nums[i]; //5
 
-      console.log(i, "i");
-      console.log(sumNum, "sum");
+    for (let j = i + 1; j < nums.length; j++) {
+      sumNum = nums[i] + nums[j]; // 5+ 5 =7
 
-    if (sumNum === target) {
-      retValue = [i, i + 1];
-      console.log(retValue, "retValue");
+      if (sumNum === target) {
+        retValue = [i, j];
+       // console.log(retValue, "retValue");
         return retValue;
+        // break;
+      }
     }
-    // else {
-    //   console.log("Target Value not found");
-    // }
-
+   
   }
 }
 
